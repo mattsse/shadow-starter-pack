70f7f16cddc4a03458936920fa10fc1ed4523c1e
Emily Hsia <emily.j.hsia@gmail.com>
2023-07-17 23:53:27 -0400

    feat(cli): Add tests for Artifacts resource

---
+++ cli/src/utils.rs
@@ -0,0 +1,13 @@
+#[macro_export]
+macro_rules! test_fixture {
+    ($cname:expr, $fname:expr) => {
+        format!(
+            "{}/src/{}/fixtures/{}",
+            env!("CARGO_MANIFEST_DIR"),
+            $cname,
+            $fname
+        )
+    };
+}
+
+pub(crate) use test_fixture;
